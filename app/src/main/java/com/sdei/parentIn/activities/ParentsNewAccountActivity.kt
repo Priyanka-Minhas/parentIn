a264d3fb1e2a3a8b4a687b8faf9e9939c4ab0ecd
Khem Singh <khem.singh@smartdatainc.net>
2019-08-20 14:24:26 +0530

    new change

--- app/src/main/java/com/sdei/parentIn/activities/ParentsNewAccountActivity.kt
+++ app/src/main/java/com/sdei/parentIn/activities/ParentsNewAccountActivity.kt
@@ -2,11 +2,18 @@ package com.sdei.parentIn.activities
 
 
 import android.content.Context
+import android.view.View
 import androidx.lifecycle.ViewModelProviders
 import com.sdei.parentIn.R
+import com.sdei.parentIn.dialog.OptionDialog
+import com.sdei.parentIn.interfaces.InterfacesCall
+import com.sdei.parentIn.utils.getGender
 import com.sdei.parentIn.viewModel.BaseViewModel
+import kotlinx.android.synthetic.main.activity_parents_new_account.*
 
-class ParentsNewAccountActivity : BaseActivity<BaseViewModel>() {
+class ParentsNewAccountActivity : BaseActivity<BaseViewModel>(), View.OnClickListener {
+
+
     override val layoutId: Int
         get() = R.layout.activity_parents_new_account
     override val viewModel: BaseViewModel
@@ -18,8 +25,22 @@ class ParentsNewAccountActivity : BaseActivity<BaseViewModel>() {
     }
 
     override fun initListeners() {
-
+        edtGender.setOnClickListener(this)
+        edtRelationshipChild.setOnClickListener(this)
+        edtLevelOfEducation.setOnClickListener(this)
+        edtNoOfStudent.setOnClickListener(this)
     }
+    override fun onClick(view: View?) {
+       when(view!!.id){
+          R.id.edtGender->{
+              OptionDialog(mContext, R.style.pullBottomfromTop, R.layout.dialog_options,
+                       getGender(),
+                       getString(R.string.select_gender),
+                       InterfacesCall.Callback { pos ->
 
+                       }).show()
+          }
+       }
+    }
 
 }
