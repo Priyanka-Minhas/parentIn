6af742e9e28396b72129014e367a82c82728f3e7
shubhamv <shubham.vema740@gmail.com>
2019-08-27 17:04:58 +0530

    init

--- app/src/main/java/com/sdei/parentIn/fragments/MessagesFragment.kt
+++ app/src/main/java/com/sdei/parentIn/fragments/MessagesFragment.kt
@@ -5,18 +5,19 @@ import com.sdei.parentIn.R
 import com.sdei.parentIn.viewModel.BaseViewModel
 
 class ParentMessageFragment : BaseFragment<BaseViewModel>() {
+
     override val layoutId: Int
         get() = R.layout.fragment_message
 
     override val viewModel: BaseViewModel
         get() = ViewModelProviders.of(this).get(BaseViewModel::class.java)
 
-
     override fun onCreateStuff() {
 
     }
 
     override fun initListeners() {
+
     }
 
 }
